eabe1c0849a4ba9d373f91b5e860958b66494d35
impossible98 <ago2chris@outlook.com>
2022-11-03 19:01:40 +0800

    feat: add Release

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/googgse/linux
 
 go 1.19
+
+require github.com/googgse/path v0.1.0 // indirect
--- linux.go
+++ linux.go
@@ -1,14 +1,79 @@
 package linux
 
 import (
+	// import built-in packages
+	"os"
+	"regexp"
 	"runtime"
+
+	// import third-party packages
+	"github.com/googgse/path"
 )
 
-// 是否为 linux
+// 是否为 Linux
 func IsLinux() bool {
+	// control flow
 	if runtime.GOOS != "linux" {
+		// return value
 		return false
 	} else {
+		// return value
 		return true
 	}
+}
+
+// 查看 Linux 发行版本
+func Release() string {
+	// control flow
+	if !IsLinux() {
+		// return value
+		return "notLinux"
+	} else if path.Exists("/etc/redhat-release") {
+		// return value
+		return "centos"
+	}
+	bytes, err := os.ReadFile("/etc/issue")
+	regDebian := regexp.MustCompile(`(?i)(^|([\s\t\n]+))(debian)($|([\s\t\n]+))`)
+	regUbuntu := regexp.MustCompile(`(?i)(^|([\s\t\n]+))(ubuntu)($|([\s\t\n]+))`)
+	regCentos := regexp.MustCompile(`(?i)(^|([\s\t\n]+))(centos)($|([\s\t\n]+))`)
+	regRedHat := regexp.MustCompile(`(?i)(^|([\s\t\n]+))(redhat)($|([\s\t\n]+))`)
+	regRedHat2 := regexp.MustCompile(`(?i)(^|([\s\t\n]+))(red hat)($|([\s\t\n]+))`)
+	// control flow
+	if err != nil {
+		bytes, err := os.ReadFile("/proc/version")
+		// control flow
+		if err != nil {
+			// return value
+			return "unknown"
+		} else if regDebian.MatchString(string(bytes)) {
+			// return value
+			return "debian"
+		} else if regUbuntu.MatchString(string(bytes)) {
+			// return value
+			return "ubuntu"
+		} else if regCentos.MatchString(string(bytes)) {
+			// return value
+			return "centos"
+		} else if regRedHat.MatchString(string(bytes)) {
+			// return value
+			return "redhat"
+		} else if regRedHat2.MatchString(string(bytes)) {
+			// return value
+			return "redhat"
+		}
+	} else if regUbuntu.MatchString(string(bytes)) {
+		// return value
+		return "ubuntu"
+	} else if regCentos.MatchString(string(bytes)) {
+		// return value
+		return "centos"
+	} else if regRedHat.MatchString(string(bytes)) {
+		// return value
+		return "redhat"
+	} else if regRedHat2.MatchString(string(bytes)) {
+		// return value
+		return "redhat"
+	}
+	// return value
+	return "unknown"
 }
